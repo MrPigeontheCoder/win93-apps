ca1d2e45214d335849ff5a7d6cb3ded108900684
MrPigeontheCoder <105377999+MrPigeontheCoder@users.noreply.github.com>
2022-08-03 18:59:44 +0200

    Update programming-docs-iFrame.js

--- apps/win93-JS-Docs/programming-docs-iFrame.js
+++ apps/win93-JS-Docs/programming-docs-iFrame.js
@@ -1,6 +1,6 @@
 $confirm('Would you like to open the Windows93 JS Docs?', function (ok){
   if (ok) {
-    $window('https://yttriferous.dev/windows93-js-docs/old.html')
+    $window('yttriferous.dev/windows93-js-docs/old.html')
   } else {
     $alert.info('Ok!')
   }
