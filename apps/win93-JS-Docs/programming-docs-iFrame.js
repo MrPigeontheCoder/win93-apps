b880f91e03fc235909696f38bc41b6634508e1c3
MrPigeontheCoder <105377999+MrPigeontheCoder@users.noreply.github.com>
2022-08-04 20:43:51 +0200

    Update programming-docs-iFrame.js
    Added another documentation website

--- apps/win93-JS-Docs/programming-docs-iFrame.js
+++ apps/win93-JS-Docs/programming-docs-iFrame.js
@@ -1,6 +1,7 @@
 $confirm('Would you like to open the Windows93 JS Docs?', function (ok){
   if (ok) {
     $window('yttriferous.dev/windows93-js-docs/old.html')
+    $window('https://itspablo.gitbook.io/windows93-docs/intro/readme')
   } else {
     $alert.info('Ok!')
   }
