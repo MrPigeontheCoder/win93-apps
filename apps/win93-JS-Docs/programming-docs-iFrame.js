21b6b2c677b288730b7f15e6e6f635319e89f488
ip07 <78528552+its-pablo@users.noreply.github.com>
2022-08-25 18:20:04 -0700

    Remove path just in case it breaks

--- apps/win93-JS-Docs/programming-docs-iFrame.js
+++ apps/win93-JS-Docs/programming-docs-iFrame.js
@@ -1,7 +1,7 @@
 $confirm('Would you like to open the Windows93 JS Docs?', function (ok){
   if (ok) {
     $window('yttriferous.dev/windows93-js-docs/old.html')
-    $window('https://itspablo.gitbook.io/windows93-docs/intro/readme')
+    $window('https://itspablo.gitbook.io/windows93-docs/')
   } else {
     $alert.info('Ok!')
   }
